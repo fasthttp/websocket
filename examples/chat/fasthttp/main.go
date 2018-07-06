c61889c57282aea1d3808df371cc6c24a92c6deb
Sergio Andres Virviescas Santana <savirviescas@qdqmedia.com>
2018-07-06 11:15:53 +0200

    Added chat example

---
+++ examples/chat/fasthttp/main.go
@@ -0,0 +1,48 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/erikdubbelboer/fasthttp"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(ctx *fasthttp.RequestCtx) {
+	log.Println(string(ctx.Path()))
+
+	if !ctx.IsGet() {
+		ctx.Error("Method not allowed", fasthttp.StatusMethodNotAllowed)
+		return
+	}
+	fasthttp.ServeFile(ctx, "../home.html")
+}
+
+func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/":
+			serveHome(ctx)
+		case "/ws":
+			serveWs(ctx, hub)
+		default:
+			ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+		}
+	}
+
+	server := fasthttp.Server{
+		Name:    "ChatExample",
+		Handler: requestHandler,
+	}
+
+	log.Fatal(server.ListenAndServe(*addr))
+}
