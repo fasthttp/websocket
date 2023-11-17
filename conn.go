0709026ab503d4bd33004e9f7a6e8a0c8d51b088
Sergio VS <savsgio.engineer@proton.me>
2023-11-17 14:42:13 +0100

    Merge branch 'main' of https://github.com/gorilla/websocket

--- conn.go
+++ conn.go
@@ -1238,7 +1238,8 @@ func (c *Conn) SetCloseHandler(h func(code int, text string) error) {
 	if h == nil {
 		h = func(code int, text string) error {
 			message := FormatCloseMessage(code, "")
-			if err := c.WriteControl(CloseMessage, message, time.Now().Add(writeWait)); err != nil {
+			err := c.WriteControl(CloseMessage, message, time.Now().Add(writeWait))
+			if err != nil && err != ErrCloseSent {
 				return err
 			}
 			return nil
